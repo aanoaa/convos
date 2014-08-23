3476c26f9958b9060b7228c1c22d9a921997ac2e
Marcus <marcus@nordaaker.com>
2014-08-23 23:34:36 +0200

    Start of proxy

---
+++ lib/Convos/Proxy.pm
@@ -0,0 +1,27 @@
+package Convos::Proxy;
+
+
+use Mojo::Base 'Mojo::IOLoop::Server';
+
+
+has tls => undef;
+has 'connection';
+
+sub start {
+  my $self = shift;
+  $self->on(
+    accept => sub {
+      my ($server, $handle) = @_;
+
+      my $stream = Mojo::IOLoop::Stream->new($handle);
+      my $irc    = Mojo::IRC->new();
+      $irc->{stream} = $stream;
+      $irc->write('AUTH');
+    ) $stream->on(read => sub { $self->_read($_[1]) });
+    }) $self->SUPER::start(@_);
+}
+
+sub _read {
+    my ($self, $id, $chunk) = @_;
+}
+1;

cc22110fb3545784025011d4800a56180ae39eac
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-01-09 09:41:29 +0100

    More padding!

--- public/js/component/timestamp.js
+++ public/js/component/timestamp.js
@@ -3,6 +3,6 @@ var React = require('react');
 var i = 2;
 module.exports = React.createClass({
   render: function() {
-    return (<span className="timestamp">{"17:0" + (i++)}</span>);
+    return (<span className="timestamp">{"17:" + (++i < 10 ? '0' + i : i)}</span>);
   }
 });
